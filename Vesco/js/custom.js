45244d610441b7194d10b1ccd7e1974b03e6806f
Alvaro Santillan <afsm1995@live.com>
2017-12-20 17:42:06 -0700

    "Our Work" section is done.
    Added magnific-popup script

--- Vesco/js/custom.js
+++ Vesco/js/custom.js
@@ -6,3 +6,20 @@ $(function() {
     // animate on scroll
     new WOW().init();
 });
+
+/*==================================================================
+                        WORK
+==================================================================*/
+
+$(function() {
+    // bug lower case function
+    // bug "(function)(){" should be (function(){
+
+    $("#work") .magnificPopup({
+        delegate: 'a', // child items selector, by clicking on it popup will open
+        type: 'image', // , to turn nclude gallery
+        gallery: {
+            enabled: true
+        }
+    });
+});
