2a6b1d480b3746c290a824e908831875741e775b
Alvaro Santillan <afsm1995@live.com>
2017-12-27 15:52:55 -0700

    Stats section complete

--- Vesco/js/custom.js
+++ Vesco/js/custom.js
@@ -18,7 +18,8 @@ $(function() {
     $("#work") .magnificPopup({
         delegate: 'a', // child items selector, by clicking on it popup will open
         type: 'image', // , to turn include gallery
-        gallery: {
+        gallery: 
+        {
             enabled: true
         }
     });
@@ -29,15 +30,15 @@ $(function() {
 ==================================================================*/
 
 $(function() {
-    
-  $("#team-members").owlCarousel({
-      items: 3,
-      autoplay: true,
-      smartSpeed: 700,
-      loop: true,
-      autoplayHoverPause: true,
-  });
-    
+
+    $("#team-members").owlCarousel({
+        items: 3,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true,
+    });
+
 });
 
 /*==================================================================
@@ -46,15 +47,38 @@ $(function() {
 
 $(function() {
     
-  $("#customers-testimonials").owlCarousel({
-    items: 1,
-    autoplay: true,
-    smartSpeed: 700,
-    loop: true,
-    autoplayHoverPause: true,
-  });
+    $("#customers-testimonials").owlCarousel({
+        items: 1,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true,
+    });
     
 });
+
+/*==================================================================
+                        STATS
+==================================================================*/
+
+$(function() {
+    
+    $('.counter').counterUp({
+        delay: 10, 
+        time: 2000
+    });
+    
+});
+
+
+
+
+
+
+
+
+
+
 
 
 
