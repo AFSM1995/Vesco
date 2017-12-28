fb5dee60259c36881dc7ef6dcf65bbf7d4bf9e26
Alvaro Santillan <afsm1995@live.com>
2017-12-27 17:56:29 -0700

    Contact section complete

--- Vesco/js/custom.js
+++ Vesco/js/custom.js
@@ -70,8 +70,21 @@ $(function() {
     
 });
 
+/*==================================================================
+                        CLIENTS
+==================================================================*/
 
+$(function() {
 
+    $("#clients-list").owlCarousel({
+        items: 6,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true,
+    });
+
+});
 
 
 
