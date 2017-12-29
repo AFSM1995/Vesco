679a1b77315623f43cec4f90fdcfc5b4c3e86e2f
Alvaro Santillan <afsm1995@live.com>
2017-12-29 15:24:43 -0700

    Website complete
    /* 167 lines down from 235 lines 28.9% line reduction */
    /* 128 lines down from 219 lines 41.6% line reduction */
    /* 583 lines down from 732 lines 20.4% line reduction */
    <!-- 895 lines down from 1252 lines 28.5% line reduction-->

--- Vesco/js/custom.js
+++ Vesco/js/custom.js
@@ -1,7 +1,6 @@
 /*==================================================================
                         SERVICES
 ==================================================================*/
-
 $(function() {
     // animate on scroll
     new WOW().init();
@@ -10,209 +9,120 @@ $(function() {
 /*==================================================================
                         WORK
 ==================================================================*/
-
 $(function() {
     // bug lower case function
     // bug "(function)(){" should be (function(){
-
     $("#work") .magnificPopup({
         delegate: 'a', // child items selector, by clicking on it popup will open
         type: 'image', // , to turn include gallery
         gallery: 
-        {
-            enabled: true
-        }
+        { enabled: true}
     });
 });
 
 /*==================================================================
                         TEAM
 ==================================================================*/
-
 $(function() {
-
     $("#team-members").owlCarousel({
         items: 3,
         autoplay: true,
         smartSpeed: 700,
         loop: true,
-        autoplayHoverPause: true,
-        
+        autoplayHoverPause: true,        
         // responsive code copied from owl carousel documintation
-        responsive: {
-            
+        responsive: {            
             // breakpoint from 0 up
-            0: { 
-                items: 1
-            },
-
+            0: {items: 1},
             // breakpoint from 480 up
-            480: {
-                items: 2
-            },
-
+            480: {items: 2},
             // breakpoint from 768 up
-            768: {
-                items: 3
-            },
-            
-        }
-        
+            768: {items: 3},            
+        }        
     });
-
 });
 
 /*==================================================================
                         TESTIMONIALS
 ==================================================================*/
-
-$(function() {
-    
+$(function() {   
     $("#customers-testimonials").owlCarousel({
         items: 1,
         autoplay: true,
         smartSpeed: 700,
         loop: true,
         autoplayHoverPause: true,
-    });
-    
+    });  
 });
 
 /*==================================================================
                         STATS
 ==================================================================*/
-
-$(function() {
-    
+$(function() {    
     $('.counter').counterUp({
         delay: 10, 
         time: 2000
-    });
-    
+    });    
 });
 
 /*==================================================================
                         CLIENTS
 ==================================================================*/
-
 $(function() {
-
     $("#clients-list").owlCarousel({
         items: 6,
         autoplay: true,
         smartSpeed: 700,
         loop: true,
-        autoplayHoverPause: true,
-        
+        autoplayHoverPause: true,        
         // responsive code copied from owl carousel documintation
-        responsive: {
-            
+        responsive: {            
             // breakpoint from 0 up
-            0: { 
-                items: 1
-            },
-
+            0: {items: 1},
             // breakpoint from 480 up
-            480: {
-                items: 3
-            },
-
+            480: {items: 3},
             // breakpoint from 768 up
-            768: {
-                items: 5
-            },
-            
+            768: {items: 5},            
             // breakpoint from 992 up
-            992: {
-                items: 6
-            }
-            
-        }
-        
+            992: {items: 6}           
+        }        
     });
-
 });
 
 /*==================================================================
                         NAVIGATION
 ==================================================================*/
-
 $(function() {
-// show/hide the navigation bar
-    
-    $(window).scroll(function() {
-                     
+// show/hide the navigation bar    
+    $(window).scroll(function() {                     
         if  ($(this).scrollTop() < 50) {
             // hide the nav bar
             $("nav").removeClass("vesco-top-nav");
-            $("#back-to-top").fadeOut();
-            
+            $("#back-to-top").fadeOut();            
         } else {
             // show the nav bar
             $("nav").addClass("vesco-top-nav");
             $("#back-to-top").fadeIn();
-        }
-        
-    });
-    
+        }        
+    });    
 });
-
 // close mobile menu on click
-
-$(function() {
-    
-    $(".navbar-collapse ul li a").on("click touch", function() {
-        
-        $(".navbar-toggle").click();
-        
-    })
-    
+$(function() {    
+    $(".navbar-collapse ul li a").on("click touch", function() {        
+        $(".navbar-toggle").click();        
+    })    
 });
-
 
 /*==================================================================
                         SMOOTH SCROLLING
 ==================================================================*/
-
-
-$(function() {
-    
-    $("a.smooth-scroll").click(function(event){
-        
-        event.preventDefault();
-        
+$(function() {    
+    $("a.smooth-scroll").click(function(event){        
+        event.preventDefault();        
         // get/return id like #about etc
-        var section = $(this).attr("href");
-        
-        $('html, body').animate({
-            
-            scrollTop: $(section).offset().top -64
-            
-        }, 1250, "easeInOutExpo"); // 1.25 secound animation
-        
-    });
-    
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        var section = $(this).attr("href");        
+        $('html, body').animate({            
+            scrollTop: $(section).offset().top -64            
+        }, 1250, "easeInOutExpo"); // 1.25 secound animation        
+    });    
+}); /* 128 lines down from 219 lines 41.6% line reduction */
