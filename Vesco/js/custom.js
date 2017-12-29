848759276787a85d34873393282f58693f5a5ec5
Alvaro Santillan <afsm1995@live.com>
2017-12-28 22:36:23 -0700

    Responsive layout complete

--- Vesco/js/custom.js
+++ Vesco/js/custom.js
@@ -37,6 +37,27 @@ $(function() {
         smartSpeed: 700,
         loop: true,
         autoplayHoverPause: true,
+        
+        // responsive code copied from owl carousel documintation
+        responsive: {
+            
+            // breakpoint from 0 up
+            0: { 
+                items: 1
+            },
+
+            // breakpoint from 480 up
+            480: {
+                items: 2
+            },
+
+            // breakpoint from 768 up
+            768: {
+                items: 3
+            },
+            
+        }
+        
     });
 
 });
